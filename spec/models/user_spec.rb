d7042a3f6329b07d71052c0baa9c50d5f4c41fb4
dgoretsky <dgoretsky@yahoo.com>
2012-04-08 18:27:38 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,8 +13,10 @@ require 'spec_helper'
 
 describe User do
   
-  before { @user = User.new(name: "Example User", email: "user@example.com",
-			    password: "foobar", password_confirmation: "foobar") }
+  before do
+	@user = User.new(name: "Example User", email: "user@example.com",
+			 password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
@@ -23,6 +25,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -108,7 +111,10 @@ describe User do
     end
   end
 
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
 
 end
